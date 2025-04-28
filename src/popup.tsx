f1a83480ad7fb893aa0d18dfda1e1e81ce26ece9
Federico Ponzi <me@fponzi.me>
2025-04-28 23:00:42 +0100

    don't send opensingletab display page to background, and focus on the page if it is opened already

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,23 +1,40 @@
 import React from "react";
-function storeTabs() {
+
+const linkUrl = `tabs/display.html`;
+async function storeTabs() {
     const query_args = {pinned: false, currentWindow: true};
     chrome.tabs.query(query_args, function (tabs) {
-        const tabUrls = tabs.map((tab) => tab.url);
+        const tabUrls = tabs
+            .filter(tab => tab.url !== chrome.runtime.getURL(linkUrl))
+            .map((tab) => tab.url);
         chrome.storage.local.set({savedTabs: tabUrls}, function () {
             console.log("Tabs stored in local storage.");
         });
     });
     chrome.tabs.query(query_args, function (tabs) {
         tabs.forEach((tab) => {
-            chrome.tabs.remove(tab.id);
+            if (tab.url !== chrome.runtime.getURL(linkUrl)) {
+                chrome.tabs.remove(tab.id);
+            }
         });
     });
-    openDisplayPage();
+    await openDisplayPage();
 }
 
-function openDisplayPage() {
-    const linkUrl = `tabs/display.html`;
-    chrome.tabs.create({url: linkUrl});
+async function openDisplayPage() {
+    // Check if a display page is already open
+    chrome.tabs.query({url: chrome.runtime.getURL(linkUrl)}, async function (tabs) {
+        if (tabs.length > 0) {
+            let tab = tabs[0];
+            // If a display page is already open, switch focus to it
+            await chrome.tabs.reload(tab.id);
+            await chrome.tabs.update(tab.id, {active: true});
+            await chrome.windows.update(tab.windowId, {focused: true});
+        } else {
+            // If no display page is open, create a new one
+            await chrome.tabs.create({url: linkUrl});
+        }
+    });
 }
 
 function IndexPopup() {
